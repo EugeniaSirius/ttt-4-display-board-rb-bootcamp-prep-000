5e1013ae23ef3ba7ed3f5e87a930fd30aabfb69c
Eugenia <eugenia.kaloshina@gmail.com>
2019-05-25 15:41:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 
 def display_board (board)
-  #board = [" "," "," "," "," "," "," "," "," "]
+  board = [" "," "," "," "," X "," "," "," "," "]
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
